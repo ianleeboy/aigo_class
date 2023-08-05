ad34c591f8a02e859c7208020b0c237156a0a726
ianleeboy <136286195+ianleeboy@users.noreply.github.com>
2023-08-05 16:02:49 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -23,7 +23,7 @@ def about_us_event(event):
         sticker_id = '52002740'
     )
 
-    about_us_img = ''
+    about_us_img = 'https://i.imgur.com/hmhKNfT.jpg'
 
     image_message = ImageSendMessage(
         original_content_url=about_us_img,
