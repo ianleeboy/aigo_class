025765b034076ca47164f3f86ccf4af494433cc1
ianleeboy <136286195+ianleeboy@users.noreply.github.com>
2023-08-05 14:59:32 +0800

    Add files via upload

---
+++ events/basic.py
@@ -0,0 +1,49 @@
+from line_bot_api import *
+
+def about_us_event(event):
+    emoji = [
+        {
+            "index": 0,
+            "productId": "5ac21e6c040ab15980c9b444",
+            "emojiId": "004"
+        },
+        {
+            "index": 13,
+            "productId": "5ac22e85040ab15980c9b44f",
+            "emojiId": "027"
+        }
+    ]
+
+    text_message = TextSendMessage(text='''$ Oh! MaMa Chicken $
+獨家特調淋醬x美式炸雞＝Oh！美味
+目前僅提供網路預訂            ''', emojis=emoji)
+    
+    sticker_message = StickerSendMessage(
+        packed_id = '11537',
+        sticker_id = '52002740'
+    )
+
+    about_us_img = ''
+
+    image_message = ImageSendMessage(
+        original_content_url=about_us_img,
+        preview_image_url=about_us_img
+    )
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, sticker_message, image_message]
+    )
+
+def location_event(event):
+    location_message = LocationSendMessage(
+        title='Oh! MaMa Chicken',
+        address='407台中市西屯區福星路523號',
+        latitude=24.177522381871487,
+        longitude=120.64528369593195
+    )
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        location_message
+    )
---
+++ line_bot_api.py
@@ -0,0 +1,12 @@
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage, LocationSendMessage
+)
+
+line_bot_api = LineBotApi('lC3x4DztwkbiuE10Thh2e15d0JXNfij2qvGkPBRgfuccrYxzmy9gkndKphWQphcIF98dJ5tPYfi1RIECue2uo98PEZhL1HfyuN3wrLqRDcOVnuAbUA3Gj3w4NtkfE42hMq9dE7L52FUd1ZDu/+egUAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('30f1790759183d5f66e968eae3ce65c0')
