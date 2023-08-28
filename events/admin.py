8f7c5673c3d1efe260997ae9cba27437502698b6
ianleeboy <136286195+ianleeboy@users.noreply.github.com>
2023-08-28 16:10:58 +0800

    Add files via upload

--- events/admin.py
+++ events/admin.py
@@ -7,8 +7,10 @@ from models.reservation import Reservation
 import datetime
 
 def list_reservation_event(event):
+    user = User.query.filter(User.line_id == event.source.user_id).first()
     reservations = Reservation.query.filter(Reservation.is_canceled.is_(False),
-                                            Reservation.booking_datetime > datetime.datetime.now()
+                                            Reservation.booking_datetime > datetime.datetime.now(),
+                                            Reservation.user_id == user.id
                                             ).order_by(Reservation.booking_datetime.asc()).all()
     
     reservation_data_text = '## 預約名單: ## \n\n'
@@ -16,7 +18,7 @@ def list_reservation_event(event):
     for reservation in reservations:
         reservation_data_text += f'''預約日期: {reservation.booking_datetime}
 預約餐點: {reservation.booking_service}
-姓名: {reservation.user.display_name}\n'''
+姓名: {user.display_name}\n'''
         
     line_bot_api.reply_message(
         event.reply_token,
